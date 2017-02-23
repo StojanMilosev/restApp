aa7bffc60487ae348acec02eeaac40ca4ef9374b
stojan.milosev <stojan.milosev@yahoo.com>
2017-02-23 15:10:53 +0100

    adding aditional modules

---
+++ storage.js
@@ -0,0 +1,95 @@
+var MongoClient = require('mongodb').MongoClient
+  		, Server = require('mongodb').Server;
+var ObjectID = require('mongodb').ObjectID;
+var database;
+var mongoClient = new MongoClient(new Server('localhost', 27017));
+module.exports = {
+	connect: function() {
+		// Connection URL
+			var url = 'mongodb://localhost:27017/twitter_notes';
+			// Use connect method to connect to the Server
+			MongoClient.connect(url, function(err, db) {
+			console.log("Connected correctly to server");
+
+			db.close();
+			});
+	},
+
+	connected: function() {
+		return typeof database != 'undefined';
+	},
+	getFriends: function(userId, cb) {
+		var cursor = database.collection('friends').find({
+			for_user: userId
+		});
+
+		cursor.toArray(cb);
+	},
+	insertFriends: function(friends) {
+		database.collection('friends').insert(friends, function(err) {
+			if (err) {
+				console.log("Cannot insert friends to database.");
+			}
+		});
+	},
+	getNotes: function(ownerId, friendId, cb) {
+		var cursor = database.collection('notes').find({
+			owner_id: ownerId,
+			friend_id: friendId
+		});
+
+		cursor.toArray(function(err,notes){
+			if (err) {
+				return cb(err);
+			}
+
+			cb(null,notes.map(function (note) {
+				return {
+					_id: note_id,
+					content: note.content
+				};
+			}));
+		});
+	},
+	insertNote: function(ownerId, friendId, content, cb) {
+		database.collection('notes').insert({
+			owner_id: ownerId,
+			friend_id: friendId,
+			content: content
+		},
+		function(err, result) {
+			if (err) {
+				return cb(err, result);
+			}
+
+			cb(null, {
+				_id: result.ops[0]._id,
+				content: result.ops[0].content
+			});
+		});
+	},
+	updateNote: function(noteId, ownerId, content, cb) {
+		database.collection('notes').updateOne({
+			_id: new ObjectID(noteId),
+			owner_id: ownerId // Used to protect other users' data
+		},
+		{
+			$set: { content: content }
+		},
+		function(err, result) {
+			if (err) {
+				return cb(err);
+			}
+
+			database.collection('notes').findOne({
+				_id: new ObjectID(noteId)
+			}, cb);
+		});
+	},
+	deleteNote: function(noteId, ownerId, cb) {
+		database.collection('notes').deleteOne({
+			_id: new ObjectID(noteId),
+			owner_id: ownerId // Used to protect other users' data
+		}, cb);
+	}
+}
